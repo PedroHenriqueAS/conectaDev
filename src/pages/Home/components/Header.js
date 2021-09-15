db1dee195f117e1a02584267b09b80a476573133
PedroHenriqueAS <ph.andradesousa31@gmail.com>
2021-09-14 23:26:56 -0300

    initial code

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,20 @@
+
+
+function Header(){
+    return(
+        <header className='header'>
+                <div  className='toolbar'>
+                    <div  className=''>
+                        <span>Conecta Dev</span>
+                    </div>
+                    <div  className=''>
+                        <button>Novo Post</button>
+                        <span>img1</span>
+                        <span>img2</span>
+                    </div>
+                </div>
+            </header>
+    )
+}
+
+export default Header;
